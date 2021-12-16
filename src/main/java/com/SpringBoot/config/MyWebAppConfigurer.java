20a81c43b29d0584473d21d6f8585ff43bb4dc29
coolkidhay <coolkidhay@foxmail.com>
2021-12-16 14:22:14 +0800

    修改配置文件

--- src/main/java/com/SpringBoot/config/MyWebAppConfigurer.java
+++ src/main/java/com/SpringBoot/config/MyWebAppConfigurer.java
@@ -1,14 +1,22 @@
 package com.SpringBoot.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * @author HuangYan
+ */
 @Configuration
 public class MyWebAppConfigurer implements WebMvcConfigurer {
+
+    @Value("${upload.file.location:}")
+    private String resourcePath;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        String filePath = "file:D:/IDEAWorkspace/images/";
+        String filePath = "file:" + resourcePath;
         registry.addResourceHandler("/images/**").addResourceLocations(filePath);
     }
 }
